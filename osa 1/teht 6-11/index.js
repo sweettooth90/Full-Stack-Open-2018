0e6a81e73c90bfe765f7159e8999df97443549b2
sweettooth90 <simo.kuparinen@helsinki.fi>
2019-08-10 23:54:21 +0300

    Update index.js

--- osa 1/teht 6-11/index.js
+++ osa 1/teht 6-11/index.js
@@ -1,80 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            good: 0,
-            neutral: 0,
-            bad: 0
-        }
-    }
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    handleAction = (value) => () => {
-        if (value === 'good') {
-            this.setState({ good: this.state.good + 1 })
-        } else if (value === 'neutral') {
-            this.setState({ neutral: this.state.neutral + 1 })
-        } else if (value === 'bad') {
-            this.setState({ bad: this.state.bad + 1 })
-        } else if (value === 'reset') {
-            this.setState({ good: 0, neutral: 0, bad: 0 })
-        }
-    }
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
 
-    render() {
-        return (
-            <div>
-                <Title title="Give feedback" />
-                <Button handleClick={this.handleAction('good')} text="Good" />&nbsp;
-                <Button handleClick={this.handleAction('neutral')} text="Neutral" />&nbsp;
-                <Button handleClick={this.handleAction('bad')} text="Bad" />&nbsp;
-                <Button handleClick={this.handleAction('reset')} text="Reset" />&nbsp;
-                <Title title="statistics" />
-                <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} />
-            </div>
-        )
-    }
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header title='give feedback' />
+      <Button handleClick={handleClickGood} text='good' />
+      <Button handleClick={handleClickNeutral} text='neutral' />
+      <Button handleClick={handleClickBad} text='bad' />
+      <Header title='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
 }
 
-const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-        {text}
-    </button>
-)
+const Header = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
 
-const Title = ({ title }) => (
-    <h2>{title}</h2>
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const StatsValues = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
-    const amount = good + neutral + bad
-    const average = ((good - bad) / amount).toFixed(1)
-    const positive = (100 * (good / amount)).toFixed(1) + "%"
-    if (good === 0 && neutral === 0 && bad === 0) {
-        return (
-            <div>no given feedback</div>
-        )
-    }
-    return (
-        <table>
-            <tbody>
-                <Statistic name={"Good"} value={good} />
-                <Statistic name={"Neutral"} value={neutral} />
-                <Statistic name={"Bad"} value={bad} />
-                <Statistic name={"Average"} value={average} />
-                <Statistic name={"Positive"} value={positive} />
-            </tbody>
-        </table>
-    )
+  const totalAmount = good + neutral + bad
+  const average = (good - bad) / totalAmount
+  const positive = 100 * (good / totalAmount) + '%'
+
+  return (
+    <table>
+      {totalAmount === 0 ?
+        'No feedback given'
+        :
+        <tbody>
+          <StatsValues text='good' value={good} />
+          <StatsValues text='neutral' value={neutral} />
+          <StatsValues text='bad' value={bad} />
+          <StatsValues text='all' value={totalAmount} />
+          <StatsValues text='average' value={average} />
+          <StatsValues text='positive' value={positive} />
+        </tbody>
+      }
+    </table>
+  )
 }
 
-const Statistic = ({ name, value }) => (
-    <tr>
-        <td>{name}&nbsp;</td>
-        <td>{value}</td>
-    </tr>
+ReactDOM.render(<App />,
+  document.getElementById('root')
 )
-
-ReactDOM.render(<App />, document.getElementById('root'))

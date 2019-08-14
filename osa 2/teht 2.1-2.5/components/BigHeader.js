8ee785f39ff886d6903bb42614f77ba0c0a1cf42
sweettooth90 <simo.kuparinen@helsinki.fi>
2019-08-14 21:45:05 +0300

    Create BigHeader.js

---
+++ osa 2/teht 2.1-2.5/components/BigHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BigHeader = () => {
+    return (
+        <div>
+            <h1>Web development curriculum</h1>
+        </div>
+    )
+}
+
+export default BigHeader

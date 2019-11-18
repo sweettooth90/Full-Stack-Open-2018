ebc63a3f3e66cf05cc0b2837ecb7422ab2dcb93d
sweettooth90 <simo.kuparinen@helsinki.fi>
2019-11-18 14:06:30 +0200

    Delete Filter.js

--- osa 2/teht 2.6-2.11/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = ({ filter, handleFilter }) => (
-    <div>
-        filter shown with <input value={filter} onChange={handleFilter} />
-    </div>
-)
-
-export default Filter

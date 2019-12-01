e56117e1bd08d27e2c118244e7a4d78d47b5704e
sweettooth90 <simo.kuparinen@helsinki.fi>
2019-12-01 17:21:47 +0200

    Update App.js

--- osa 2/puhelinluettelo/src/App.js
+++ osa 2/puhelinluettelo/src/App.js
@@ -2,22 +2,18 @@ import React, { useEffect, useState } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import Form from './components/Form'
-import axios from 'axios'
+import personService from './services/PersonService'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/persons')
-            .then(response => {
-                setPersons(response.data)
-            })
+        personService
+            .getAll()
+            .then(initialPersons => setPersons(initialPersons))
     }, [])
 
     const handleFilter = (event) => (
@@ -34,18 +30,59 @@ const App = () => {
 
     const addName = (event) => {
         event.preventDefault()
-        if (persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase())) {
-            alert(`${newName} is already added to phonebook`)
-            setNewName('')
-            setNewNumber('')
-        } else {
-            const personObject = {
-                name: newName,
-                number: newNumber
+
+        const personObject = {
+            name: newName,
+            number: newNumber
+        }
+
+        if (persons.map(person => person.name.toLowerCase()).indexOf(newName.toLowerCase()) !== -1) {
+
+            if (window.confirm(
+                `${newName} is already added to phonebook, replace the old number with a new one?`)
+            ) {
+                const editPerson = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
+
+                const updatedPerson = { ...editPerson, number: newNumber }
+
+                personService
+                    .update(updatedPerson.id, updatedPerson)
+                    .then(newPerson => {
+                        setPersons(persons.map(person => person.id !== editPerson.id ? person : newPerson))
+                        setNewName('')
+                        setNewNumber('')
+                        alert(`Updated ${newName}`)
+                    })
+                    .catch(error => {
+                        setPersons(persons.filter(persons => persons.id !== editPerson.id))
+                        alert(`${newName} has been removed.`)
+                    })
             }
-            setPersons(persons.concat(personObject))
-            setNewName('')
-            setNewNumber('')
+        } else {
+            personService
+                .create(personObject)
+                .then(newPerson => {
+                    setPersons(persons.concat(newPerson))
+                    setNewName('')
+                    setNewNumber('')
+                    alert(`Added ${newName}`)
+                })
+
+        }
+    }
+
+    const removeName = id => {
+        const removePerson = persons.find(person => person.id === id)
+        if (window.confirm(`Do you want to delete ${removePerson.name}?`)) {
+            personService
+                .remove(id)
+                .then(() => {
+                    setPersons(persons.filter(person => person.id !== id))
+                })
+                .catch(error => {
+                    setPersons(persons.filter(person => person.id !== id))
+                    alert(`${removePerson.name} has already been removed.`)
+                })
         }
     }
 
@@ -62,7 +99,7 @@ const App = () => {
                 handleNumberChange={handleNumberChange}
             />
             <h2>Numbers</h2>
-            <Persons persons={persons} filter={filter} />
+            <Persons persons={persons} filter={filter} handleClick={removeName} />
         </div>
     )
 
